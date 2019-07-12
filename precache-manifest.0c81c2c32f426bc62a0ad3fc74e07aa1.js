c1a62982d6dcd3d294ba41767dcb713f94774f7e
Catalin Bora <catalin.bora@gmail.com>
2019-07-12 23:40:00 +0300

    Updates

---
+++ precache-manifest.0c81c2c32f426bc62a0ad3fc74e07aa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3b1fd3a2a053af3085844f5335c348",
+    "url": "/comments_count/index.html"
+  },
+  {
+    "revision": "b4237fd446f995eee1d8",
+    "url": "/comments_count/static/css/main.62da17b3.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/comments_count/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "b4237fd446f995eee1d8",
+    "url": "/comments_count/static/js/main.3a4c5f42.chunk.js"
+  },
+  {
+    "revision": "3b08bd8b455d8b84c534",
+    "url": "/comments_count/static/js/runtime~main.65b357b2.js"
+  }
+]);
