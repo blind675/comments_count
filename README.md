2ff6603f8c29e0ad371737f18f0e018dfe4556c5
Bob <catalin.bora@gmail.com>
2019-07-25 10:52:20 +0300

    Addes eslint and other improvements.

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 TODOs:
 
-- add Prettier ( https://github.com/prettier/prettier )
-- add eslinter
-- add a field to get the last x posts. ( max 10 - default 1 ??)
 - get all comments for the first post
-- structure the code
--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      facebookToken: '104209816294440|xVZhfKIU2_TsCnsimmUoaM0YU68',
-      pageName: 'dominicprimar',
-      status: 'waiting',
-      postMessage: '',
-      commentsList: [],
+      facebookToken: "104209816294440|xVZhfKIU2_TsCnsimmUoaM0YU68",
+      pageName: "dominicprimar",
+      status: "waiting",
+      postMessage: "",
+      commentsList: []
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -23,8 +23,8 @@ class App extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    if (this.state.pageName === '') {
-      alert('Please provide a facebook page name!');
+    if (this.state.pageName === "") {
+      alert("Please provide a facebook page name!");
     } else {
       const pageName = this.state.pageName;
       const FBToken = this.state.facebookToken;
@@ -32,69 +32,64 @@ class App extends React.Component {
       this.setState({
         status: `getting post from page - ${pageName}`
       });
-      this.fetchPostForPage(pageName, FBToken)
+      this.fetchPostForPage(pageName, FBToken);
     }
   }
 
   fetchPostForPage(pageName, facebookToken) {
-    //get the last post of the facebook page 
+    //get the last post of the facebook page
     const url = `https://graph.facebook.com/v2.9/${pageName}/posts?limit=1&access_token=${facebookToken}`;
 
     fetch(url)
-      .then((res) => {
-        console.log('result: ', res);
+      .then(res => {
+        console.log("result: ", res);
         if (res.ok === false && res.status === 404) {
-          this.setState({ status: 'wrong facebook page name' })
+          this.setState({ status: "wrong facebook page name" });
         } else {
-          return res.json()
+          return res.json();
         }
       })
-      .then((jsonResult) => {
+      .then(jsonResult => {
         if (jsonResult && jsonResult.data) {
-          console.log(' json result: ', jsonResult);
+          console.log(" json result: ", jsonResult);
           this.setState({
             postMessage: jsonResult.data[0].message,
-            status: 'getting comments for the last post'
-          })
+            status: "getting comments for the last post"
+          });
           this.fetchCommentsForAPost(jsonResult.data[0].id, facebookToken);
         }
-      })
+      });
   }
 
   fetchCommentsForAPost(postId, facebookToken) {
     const url = `https://graph.facebook.com/v2.9/${postId}?fields=comments{message,comment_count,comments}&access_token=${facebookToken}`;
 
     fetch(url)
-      .then((res) => {
-        console.log('result: ', res);
+      .then(res => {
+        console.log("result: ", res);
         if (res.ok === false && res.status === 404) {
-          this.setState({ status: 'error getting comments' })
+          this.setState({ status: "error getting comments" });
         } else {
-          return res.json()
+          return res.json();
         }
       })
-      .then((jsonResult) => {
-
+      .then(jsonResult => {
         if (jsonResult && jsonResult.comments && jsonResult.comments.data) {
           const localResponseList = jsonResult.comments.data;
-          console.log('json result: ', localResponseList);
+          console.log("json result: ", localResponseList);
 
           this.setState({
             commentsList: localResponseList,
-            status: 'waiting'
-          })
+            status: "waiting"
+          });
         }
-      })
+      });
   }
 
   renderList(list) {
-    const listItems = list.map((item) =>
-      <li key={item.id}>{item.message}</li>
-    );
+    const listItems = list.map(item => <li key={item.id}>{item.message}</li>);
 
-    return (
-      <ul> {listItems} </ul>
-    );
+    return <ul> {listItems} </ul>;
   }
 
   render() {
@@ -104,10 +99,10 @@ class App extends React.Component {
           <form
             onSubmit={this.handleSubmit}
             style={{
-              alignItems: 'flex-start'
+              alignItems: "flex-start"
             }}
           >
-            <label >
+            <label>
               Facebook Token:
               <input
                 type="text"
@@ -115,7 +110,7 @@ class App extends React.Component {
                 style={{
                   width: 290,
                   height: 12,
-                  marginLeft: '16px',
+                  marginLeft: "16px"
                 }}
                 value={this.state.facebookToken}
                 onChange={this.handleChange}
@@ -125,7 +120,7 @@ class App extends React.Component {
             <label
               style={{
                 fontSize: 10,
-                color: 'gray'
+                color: "gray"
               }}
             >
               Don't change this unless you know what you are doing!
@@ -140,15 +135,18 @@ class App extends React.Component {
                 style={{
                   width: 200,
                   height: 16,
-                  margin: '16px',
+                  margin: "16px"
                 }}
                 value={this.state.pageName}
                 onChange={this.handleChange}
               />
             </label>
-            <input type="submit" value="Import" style={{
-              height: 16
-            }}
+            <input
+              type="submit"
+              value="Import"
+              style={{
+                height: 16
+              }}
             />
           </form>
           <label> Status: {this.state.status}</label>
